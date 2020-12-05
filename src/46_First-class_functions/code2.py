e9fb921314eba9f1fc1005836c92d2b0269d2a0f
Pedro Ivo Tostes <pedrotostes88@gmail.com>
2020-12-05 12:43:02 -0300

    Python refresher 46

---
+++ src/46_First-class_functions/code2.py
@@ -0,0 +1,22 @@
+from operator import itemgetter
+
+def search(sequence, expected, finder):
+    for elemen in sequence:
+        if finder(elemen) == expected:
+            return elemen
+    raise RuntimeError(f"Could not find an element with {expected}.")
+
+
+friends = [
+    {"name": "Bob", "age": 24},
+    {"name": "Rolf", "age": 27},
+    {"name": "Jen", "age": 30}
+]
+
+
+def get_name_friend(friend: str) -> dict:
+    return friend["name"]
+
+
+print(search(friends, "Bob", get_name_friend))
+print(search(friends, "Jen", itemgetter("name")))
